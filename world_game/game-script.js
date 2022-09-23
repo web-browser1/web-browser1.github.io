c5f9113b9f2fa98ecd0501f27ef0091acc4aa751
web-browser1 <1374904+web-browser1@users.noreply.github.com>
2022-09-23 19:13:51 +0200

    Update game-script.js

--- world_game/game-script.js
+++ world_game/game-script.js
@@ -743,7 +743,7 @@ function game_loop() {
 
  // Create gradient
 
- var grd = ctx.createLinearGradient(0, 0, 0, HEIGHT);
+ var grd = ctx.createLinearGradient(0, 0, 0, canvas.height);
  grd.addColorStop(0, 'hsl('+((hsl_h-180))+'deg '+(hsl_s+30)+'% '+((hsl_l-sunB*0.1)+45)+'%)');
  grd.addColorStop(1, 'hsl('+(hsl_h)+'deg '+(hsl_s+30+sunB)+'% '+((hsl_l+sunB*0.2)+45)+'%)');
 
